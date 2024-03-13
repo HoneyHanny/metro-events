c6e6b3191c9d9fc435e01b9834151d28e6ed234c
HoneyHanny <hansduran25@gmail.com>
2024-03-13 12:17:16 +0800

    Add possible pages files

---
+++ src/Pages/CreateEvent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// Organizers can create event in this page
+// add descriptions and details to the event
+const CreateEvent = () => {
+  return <div>CreateEvent</div>
+}
+
+export default CreateEvent
---
+++ src/Pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// NOTE(hans): Login template
+// https://github.com/mui/material-ui/tree/v5.15.12/docs/data/material/getting-started/templates/sign-up
+const Login = () => {
+  return <div>Login</div>
+}
+
+export default Login
