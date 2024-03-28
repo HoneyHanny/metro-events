72a2d821e4c8068451215c1022409b569f0ec707
John Cadungog <john.cadungog@cit.edu>
2024-03-28 17:31:31 +0800

    Update Events Card

--- src/components/eventscard.jsx
+++ src/components/eventscard.jsx
@@ -11,10 +11,14 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
+import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Button from '@mui/material/Button';
-import basketball from '../assets/images/basketball.jpg';
+import ModeCommentOutlinedIcon from '@mui/icons-material/ModeCommentOutlined';
+
+
+
+
 
 const ExpandMore = styled((props) => {
     const { expand, ...other } = props;
@@ -41,100 +45,71 @@ export default function RecipeReviewCard() {
     };
 
     return (
-        <StyledCard
-            sx={{ maxWidth: 1000, elevation: 0 }}
-            style={{ boxShadow: 'none' }}
-        >
+        <StyledCard sx={{ maxWidth: 1000, elevation: 0 }}>
+         
             <CardHeader
+              
                 avatar={
-                    <Avatar sx={{ bgcolor: red[500] }} aria-label='recipe'>
-                        R
+                    <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                        
                     </Avatar>
                 }
+          
                 action={
-                    <IconButton sx={{ height: '40px', width: '40px' }}>
-                        <MoreVertIcon />
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <span style={{ marginRight: '1px' }}>10</span> {/* Number outside the IconButton */}
+                    <IconButton sx={{ height: "40px", width: "40px", marginRight: "5px", color: '#6A6A6A' }}>
+                       <PeopleAltOutlinedIcon />
                     </IconButton>
+                   </div>
                 }
-                title='Shrimp and Chorizo Paella'
-                subheader='September 14, 2016'
+                title="Shrimp and Chorizo Paella"
+                subheader="September 14, 2016"
             />
             <CardMedia
-                component='img'
-                height='194'
-                image={basketball}
-                alt='Basketball Vent'
+                component="img"
+                height="194"
+                image="../src/assets/images/basketball.jpg"
+                alt="Basketball Vent"
             />
             <CardContent>
-                <Typography variant='body2' color='text.secondary'>
-                    This impressive paella is a perfect party dish and a fun
-                    meal to cook together with your guests. Add 1 cup of frozen
-                    peas along with the mussels, if you like.
+                <Typography variant="body2" color="text.secondary">
+                    This impressive paella is a perfect party dish and a fun meal to cook
+                    together with your guests. Add 1 cup of frozen peas along with the mussels,
+                    if you like.
                 </Typography>
             </CardContent>
             <CardActions disableSpacing>
-                <IconButton sx={{ height: '40px', width: '40px' }}>
+
+                <IconButton sx={{ height: "40px", width: "40px" }}>
                     <FavoriteIcon />
                 </IconButton>
-                <IconButton sx={{ height: '40px', width: '40px' }}>
-                    <ShareIcon />
+                <IconButton sx={{ height: "40px", width: "40px" }}>
+                    <ModeCommentOutlinedIcon />
                 </IconButton>
 
                 <Button
-                    variant='contained'
-                    color='primary'
+                    variant="contained"
+                    color="primary"
                     sx={{
                         backgroundColor: '#596EC6',
-                        borderRadius: '10px',
+                        borderRadius: '15px',
                         padding: '6px',
-                        minWidth: '40px',
+                        minWidth: '30px',
+                        minHeight: '20px',
+                        marginRight: '5px',
+                        elevation: '0',
+                        marginLeft: 'auto', // This line pushes the button to the right
                         '&:hover': {
                             backgroundColor: '#455990', // Optional: Change hover color
                         },
                     }}
-                    onClick={handleExpandClick}
-                    aria-expanded={expanded}
-                    aria-label='show more'
                 >
                     Join
                 </Button>
             </CardActions>
 
-            <Collapse in={expanded} timeout='auto' unmountOnExit>
-                <CardContent>
-                    <Typography paragraph>Method:</Typography>
-                    <Typography paragraph>
-                        Heat 1/2 cup of the broth in a pot until simmering, add
-                        saffron and set aside for 10 minutes.
-                    </Typography>
-                    <Typography paragraph>
-                        Heat oil in a (14- to 16-inch) paella pan or a large,
-                        deep skillet over medium-high heat. Add chicken, shrimp
-                        and chorizo, and cook, stirring occasionally until
-                        lightly browned, 6 to 8 minutes. Transfer shrimp to a
-                        large plate and set aside, leaving chicken and chorizo
-                        in the pan. Add pimentón, bay leaves, garlic, tomatoes,
-                        onion, salt and pepper, and cook, stirring often until
-                        thickened and fragrant, about 10 minutes. Add saffron
-                        broth and remaining 4 1/2 cups chicken broth; bring to a
-                        boil.
-                    </Typography>
-                    <Typography paragraph>
-                        Add rice and stir very gently to distribute. Top with
-                        artichokes and peppers, and cook without stirring, until
-                        most of the liquid is absorbed, 15 to 18 minutes. Reduce
-                        heat to medium-low, add reserved shrimp and mussels,
-                        tucking them down into the rice, and cook again without
-                        stirring, until mussels have opened and rice is just
-                        tender, 5 to 7 minutes more. (Discard any mussels that
-                        don&apos;t open.)
-                    </Typography>
-                    <Typography>
-                        Set aside off of the heat to let rest for 10 minutes,
-                        and then serve.
-                    </Typography>
-                </CardContent>
-            </Collapse>
+
         </StyledCard>
     );
 }
