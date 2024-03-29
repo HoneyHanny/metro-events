de0da1bcb8ebb26ce03a517403aae1c17ec9f80e
HoneyHanny <hansduran25@gmail.com>
2024-03-30 00:30:17 +0800

    Fix padding

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,76 +1,81 @@
-import React, { useEffect, useState } from "react";
-import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
-import Navbar from "../components/navbar";
-import "../css/home.css";
-import EventsCard from "../components/cards/eventscard";
-import Notifications from "../components/notifications";
-import NotificationCard from "../components/cards/notificationcard";
-import Acceptrequests from "../components/acceptrequests";
-import Joinrequests from "../components/joinrequests";
-import axios from "axios";
-import ResponsiveAppBar from "./../components/navbar";
+import React, { useEffect, useState } from 'react';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Navbar from '../components/navbar';
+import '../css/home.css';
+import EventsCard from '../components/cards/eventscard';
+import Notifications from '../components/notifications';
+import NotificationCard from '../components/cards/notificationcard';
+import Acceptrequests from '../components/acceptrequests';
+import Joinrequests from '../components/joinrequests';
+import axios from 'axios';
+import ResponsiveAppBar from './../components/navbar';
 const Home = () => {
-	const [events, setEvents] = useState([]);
+    const [events, setEvents] = useState([]);
 
-	useEffect(() => {
-		const retrieve = async () => {
-			try {
-				let response = await axios.get("http://localhost:8000/api/event/");
-				setEvents(response.data);
-				console.log(response);
-			} catch (err) {
-				console.error(err);
-			}
-		};
+    useEffect(() => {
+        const retrieve = async () => {
+            try {
+                let response = await axios.get(
+                    'http://localhost:8000/api/event/'
+                );
+                setEvents(response.data);
+                console.log(response);
+            } catch (err) {
+                console.error(err);
+            }
+        };
 
-		retrieve();
-	});
+        retrieve();
+    }, []);
 
-	return (
-		<>
-			<Navbar />
-			<div className="body">
-				<Grid container spacing={4}>
-					{/*left column */}
-					<Notifications />
+    return (
+        <>
+            <Navbar />
+            <div className='body' style={{ userSelect: 'none' }}>
+                <Grid container spacing={4}>
+                    {/*left column */}
+                    <Notifications />
 
-					{/* Middle column */}
+                    {/* Middle column */}
 
-					<Grid item xs={20} md={6}>
-						<h3>Events Feed</h3>
-						<Paper
-							elevation={3}
-							sx={{
-								borderRadius: "40px",
-								padding: "20px",
-								height: "100vh",
-								marginTop: "10px",
-							}}>
-							{/* Content for middle column */}
-							{/* Map the response */}
-							{events.map((event) => {
-								return (
-									<EventsCard
-										title={event.eventName}
-										date={event.eventDate}
-										description={event.eventDescription}
-										numOfAttendees={event.eventNumberOfAttendees}
-									/>
-								);
-							})}
-						</Paper>
-					</Grid>
+                    <Grid item xs={20} md={6}>
+                        <h3>Events Feed</h3>
+                        <Paper
+                            elevation={3}
+                            sx={{
+                                borderRadius: '40px',
+                                padding: '20px',
+                                height: '100vh',
+                                marginTop: '10px',
+                            }}
+                        >
+                            {/* Content for middle column */}
+                            {/* Map the response */}
+                            {events.map((event) => {
+                                return (
+                                    <EventsCard
+                                        title={event.eventName}
+                                        date={event.eventDate}
+                                        description={event.eventDescription}
+                                        numOfAttendees={
+                                            event.eventNumberOfAttendees
+                                        }
+                                    />
+                                );
+                            })}
+                        </Paper>
+                    </Grid>
 
-					{/* Right column */}
-					<Grid item xs={16} md={3}>
-						<Acceptrequests />
-						<Joinrequests />
-					</Grid>
-				</Grid>
-			</div>
-		</>
-	);
+                    {/* Right column */}
+                    <Grid item xs={16} md={3}>
+                        <Acceptrequests />
+                        <Joinrequests />
+                    </Grid>
+                </Grid>
+            </div>
+        </>
+    );
 };
 
 export default Home;
--- src/components/cards/acceptrequestcard.jsx
+++ src/components/cards/acceptrequestcard.jsx
@@ -7,118 +7,126 @@ import { red, blueGrey } from '@mui/material/colors';
 import Button from '@mui/material/Button';
 
 const AcceptRequestsCard = ({ image, heading, subheading, description }) => {
-  return (
-    <Card
-      sx={{
-        maxWidth: 1000,
-        minHeight: 190,
-        maxHeight: 500,
-        borderRadius: 8,
-        marginTop: 2,
-        elevation: 'none',
-        backgroundColor: '#F0F0F0',
-
-        '@media (max-width: 960px)': {
-          maxWidth: 500,
-          flexDirection: 'column',
-        },
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}
-      >
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginLeft: '15px',
-          }}
-        >
-          <Avatar
+    return (
+        <Card
             sx={{
-              bgcolor: red[500],
-              width: '80px',
-              height: '80px',
-              borderRadius: '30px',
+                padding: 1.5,
+                maxWidth: 1000,
+                minHeight: 190,
+                maxHeight: 500,
+                borderRadius: 8,
+                marginTop: 2,
+                elevation: 'none',
+                backgroundColor: '#F0F0F0',
+
+                '@media (max-width: 960px)': {
+                    maxWidth: 500,
+                    flexDirection: 'column',
+                },
             }}
-            aria-label='recipe'
-          ></Avatar>
-        </div>
-
-        <CardContent sx={{ flex: '1' }}>
-          <Typography
-            gutterBottom
-            variant='h10'
-            marginTop='10px'
-            component='div'
-            color='#596EC6'
-            fontWeight='bold'
-          >
-            JOHN CADUNGOG
-          </Typography>
-
-          <Typography
-            variant='subtitle2'
-            marginTop='-10px'
-            gutterBottom
-            fontWeight='bold'
-            color='#6A6A6A'
-          >
-            Wants to join {Event}
-          </Typography>
-          <Typography
-            variant='body2'
-            color='text.secondary'
-            minWidth='200px'
-            maxWidth='200px'
-            maxHeight='40px'
-            overflow='hidden'
-            textOverflow='ellipsis'
-          >
-            {description}
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
-            risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
-            nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas
-            ligula massa, varius a, semper congue, euismod non, mi. Proin
-            porttitor, orci nec nonummy molestie, enim est eleifend mi, non
-            fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa,
-            scelerisque vitae, consequat in, pretium a, enim.
-          </Typography>
-        </CardContent>
-      </div>
-
-      <div
-        style={{ display: 'flex', justifyContent: 'center', marginTop: '5px' }}
-      >
-        <Button
-          variant='contained'
-          style={{
-            backgroundColor: '#596EC6',
-            color: 'white',
-            marginRight: '10px',
-            borderRadius: ' 20px',
-          }}
         >
-          Accept
-        </Button>
-        <Button
-          variant='contained'
-          style={{
-            backgroundColor: '#BEC6E7',
-            color: 'white',
-            borderRadius: '20px',
-          }}
-        >
-          Decline
-        </Button>
-      </div>
-    </Card>
-  );
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                }}
+            >
+                <div
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        marginLeft: '15px',
+                    }}
+                >
+                    <Avatar
+                        sx={{
+                            bgcolor: red[500],
+                            width: '80px',
+                            height: '80px',
+                            borderRadius: '30px',
+                        }}
+                        aria-label='recipe'
+                    ></Avatar>
+                </div>
+
+                <CardContent sx={{ flex: '1' }}>
+                    <Typography
+                        gutterBottom
+                        variant='h10'
+                        marginTop='10px'
+                        component='div'
+                        color='#596EC6'
+                        fontWeight='bold'
+                    >
+                        JOHN CADUNGOG
+                    </Typography>
+
+                    <Typography
+                        variant='subtitle2'
+                        marginTop='-10px'
+                        gutterBottom
+                        fontWeight='bold'
+                        color='#6A6A6A'
+                    >
+                        Wants to join {Event}
+                    </Typography>
+                    <Typography
+                        variant='body2'
+                        color='text.secondary'
+                        minWidth='170px'
+                        maxWidth='170px'
+                        maxHeight='40px'
+                        overflow='hidden'
+                        textOverflow='ellipsis'
+                    >
+                        {description}
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Sed non risus. Suspendisse lectus tortor, dignissim sit
+                        amet, adipiscing nec, ultricies sed, dolor. Cras
+                        elementum ultrices diam. Maecenas ligula massa, varius
+                        a, semper congue, euismod non, mi. Proin porttitor, orci
+                        nec nonummy molestie, enim est eleifend mi, non
+                        fermentum diam nisl sit amet erat. Duis semper. Duis
+                        arcu massa, scelerisque vitae, consequat in, pretium a,
+                        enim.
+                    </Typography>
+                </CardContent>
+            </div>
+
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    marginTop: '5px',
+                    gap: '10px',
+                }}
+            >
+                <Button
+                    variant='contained'
+                    style={{
+                        backgroundColor: '#596EC6',
+                        color: 'white',
+                        // marginRight: '10px',
+                        borderRadius: ' 20px',
+                    }}
+                >
+                    Accept
+                </Button>
+                <Button
+                    variant='contained'
+                    style={{
+                        backgroundColor: '#BEC6E7',
+                        color: 'white',
+                        borderRadius: '20px',
+                    }}
+                >
+                    Decline
+                </Button>
+            </div>
+        </Card>
+    );
 };
 
 export default AcceptRequestsCard;
